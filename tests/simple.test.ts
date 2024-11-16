ce153b5e9e780b476407b8a99fdd449667857cbf
EarningsCall <dev@earningscall.biz>
2024-11-16 16:32:13 -0600

    Initial code push.

---
+++ tests/simple.test.ts
@@ -0,0 +1,9 @@
+describe('Basic Test Suite', () => {
+  test('should pass a simple test', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  test('should verify true is true', () => {
+    expect(true).toBe(true);
+  });
+});
