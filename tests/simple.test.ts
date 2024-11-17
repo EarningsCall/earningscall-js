ba6cc142052da8931e577b631e6c97fb361eeca2
EarningsCall <dev@earningscall.biz>
2024-11-16 20:32:20 -0600

    Trying to get basic library up and running.
    Run: `npx typescript-starter`

--- tests/simple.test.ts
+++
@@ -1,9 +0,0 @@
-describe('Basic Test Suite', () => {
-  test('should pass a simple test', () => {
-    expect(2 + 2).toBe(4);
-  });
-
-  test('should verify true is true', () => {
-    expect(true).toBe(true);
-  });
-});
