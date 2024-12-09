de78a19b1cc06e232d02572937cd588ee21413d2
EarningsCall <99980357+EarningsCall@users.noreply.github.com>
2024-12-09 14:27:33 -0600

    Add level 2 and 3 logic. (#16)

--- src/lib/company.spec.ts
+++ src/lib/company.spec.ts
@@ -4,14 +4,94 @@ import { getCompany } from './company';
 const symbolsResponseText =
   '1\tABC\tABC Test Company Inc.\t9\t30\n1\tDEF\tDEF Test Company Inc.\t9\t122';
 
-const transcriptResponseText = {
+const transcriptResponseJson = {
   event: {
     year: 2022,
     quarter: 1,
     conference_date: '2022-01-01',
-    conference_url: 'https://example.com/abc-test-company-inc-earnings-call',
   },
   text: 'Good day and welcome to the ABC Test Company Inc. Q1 FY 2022 earnings conference call.',
+};
+
+const transcriptLevel2ResponseJson = {
+  event: {
+    year: 2022,
+    quarter: 1,
+    conference_date: '2022-01-01',
+  },
+  speakers: [
+    {
+      speaker: 'spk03',
+      text: "Good day and welcome to the ABC Test Company Inc. Q1 FY 2022 earnings conference call. Today's call is being recorded. Please go ahead.",
+    },
+    {
+      speaker: 'spk15',
+      text: 'Thank you. Good afternoon, and thank you for joining us. I am the awesome CEO John Smith.',
+    },
+  ],
+  speaker_name_map_v2: {
+    spk03: {
+      name: 'Operator',
+      title: 'Host',
+    },
+    spk15: {
+      name: 'John Smith',
+      title: 'CEO',
+    },
+  },
+};
+
+const transcriptLevel3ResponseJson = {
+  event: {
+    year: 2022,
+    quarter: 1,
+    conference_date: '2022-01-01',
+  },
+  speakers: [
+    {
+      speaker: 'spk03',
+      words: [
+        'Good',
+        'day',
+        'and',
+        'welcome',
+        'to',
+        'the',
+        'ABC',
+        'Test',
+        'Company',
+        'Inc.',
+        'Q1',
+        'FY',
+        '2022',
+        'earnings',
+        'conference',
+        'call.',
+      ],
+    },
+    {
+      speaker: 'spk15',
+      words: [
+        'Thank',
+        'you',
+        'Good',
+        'afternoon',
+        'and',
+        'thank',
+        'you',
+        'for',
+        'joining',
+        'us',
+        'I',
+        'am',
+        'the',
+        'awesome',
+        'CEO',
+        'John',
+        'Smith.',
+      ],
+    },
+  ],
 };
 
 beforeAll(() => {
@@ -31,7 +111,27 @@ beforeAll(() => {
       return Promise.resolve({
         ok: true,
         status: 200,
-        json: () => transcriptResponseText,
+        json: () => transcriptResponseJson,
+      });
+    }
+    if (
+      url ===
+      'https://v2.api.earningscall.biz/transcript?apikey=demo&exchange=NASDAQ&symbol=ABC&year=2022&quarter=1&level=2'
+    ) {
+      return Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => transcriptLevel2ResponseJson,
+      });
+    }
+    if (
+      url ===
+      'https://v2.api.earningscall.biz/transcript?apikey=demo&exchange=NASDAQ&symbol=ABC&year=2022&quarter=1&level=3'
+    ) {
+      return Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => transcriptLevel3ResponseJson,
       });
     }
 
@@ -70,6 +170,10 @@ describe('company', () => {
     );
 
     await expect(
+      company.getTranscript({ year: 2022, quarter: 0 }),
+    ).rejects.toThrow('Invalid quarter. Must be one of: {1,2,3,4}');
+
+    await expect(
       company.getTranscript({ year: 2022, quarter: 1, level: 5 }),
     ).rejects.toThrow('Invalid level. Must be one of: {1,2,3}');
 
@@ -94,39 +198,38 @@ describe('company', () => {
       'Good day and welcome to the ABC Test Company Inc. Q1 FY 2022 earnings conference call.',
     );
   });
+
+  test('get transcript level 2', async () => {
+    const company = await getCompany('ABC');
+
+    const transcript = await company.getTranscript({
+      year: 2022,
+      quarter: 1,
+      level: 2,
+    });
+
+    expect(transcript.event.year).toBe(2022);
+    expect(transcript.event.quarter).toBe(1);
+    expect(transcript.event.conference_date).toBe('2022-01-01');
+    expect(transcript.text).toBe(
+      "Good day and welcome to the ABC Test Company Inc. Q1 FY 2022 earnings conference call. Today's call is being recorded. Please go ahead. Thank you. Good afternoon, and thank you for joining us. I am the awesome CEO John Smith.",
+    );
+  });
+
+  test('get transcript level 3', async () => {
+    const company = await getCompany('ABC');
+
+    const transcript = await company.getTranscript({
+      year: 2022,
+      quarter: 1,
+      level: 3,
+    });
+
+    expect(transcript.event.year).toBe(2022);
+    expect(transcript.event.quarter).toBe(1);
+    expect(transcript.event.conference_date).toBe('2022-01-01');
+    expect(transcript.text).toBe(
+      'Good day and welcome to the ABC Test Company Inc. Q1 FY 2022 earnings conference call. Thank you Good afternoon and thank you for joining us I am the awesome CEO John Smith.',
+    );
+  });
 });
-
-// const symbolsResponseText =
-//   '1\tAAPL\tApple Inc.\t9\t30\n1\tMSFT\tMicrosoft Corporation\t9\t122';
-
-// test.before(() => {
-//   global.fetch = ((url: string) => {
-//     if (url === 'https://v2.api.earningscall.biz/symbols-v2.txt?apikey=demo') {
-//       return {
-//         status: 200,
-//         text: () => symbolsResponseText,
-//       };
-//     }
-//     // if (url === 'https://v2.api.earningscall.biz/somethingelse?apikey=demo') {
-//     //   return Promise.resolve({
-//     //     status: 200,
-//     //     text: () => symbolsResponseText,
-//     //   }) as Response;
-//     // }
-//     return Promise.reject(new Error(`Unhandled request: ${url}`));
-//   }) as typeof global.fetch;
-// });
-
-// test.after(() => {
-//   delete (global as typeof globalThis).fetch;
-// });
-
-// // test('getCompany2', async (t) => {
-// //   const company = await getCompany('AAPL');
-// //   t.is(company.name, 'Apple Inc.');
-// //   t.is(company.companyInfo.exchange, 'NASDAQ');
-// //   t.is(company.companyInfo.symbol, 'AAPL');
-// //   t.is(company.companyInfo.sector, 'Technology');
-// //   t.is(company.companyInfo.industry, 'Consumer Electronics');
-// //   t.is(company.toString(), 'Apple Inc.');
-// // });
--- src/types/transcript.d.ts
+++ src/types/transcript.d.ts
@@ -1,6 +1,23 @@
 import { EarningsEvent } from './event.d';
 
+export type SpeakerInfo = {
+  name?: string;
+  title?: string;
+};
+
+export type Speaker = {
+  speaker: string;
+  speaker_info?: SpeakerInfo;
+  text?: string;
+  words?: string[];
+  start_times?: number[];
+};
+
 export type Transcript = {
-  event: EarningsEvent;
-  text: string;
+  event?: EarningsEvent;
+  text?: string;
+  speakers?: Speaker[];
+  prepared_remarks?: string;
+  questions_and_answers?: string;
+  speaker_name_map_v2?: { [key: string]: SpeakerInfo };
 };
